3a770b463185d982dc8feb2ef90bc02cc978b5f9
Ekan <ekanessien@gmail.com>
2025-03-22 09:45:10 -0700

    Style Landing page and Create Slideshow

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ setInterval(() => {
   i++;
 
   const icon = document.querySelector(".section-1-icons .change");
+
   icon.classList.remove("change");
 
   if (i > icons.length) {
-    icons[0.classList.add("change") {
-        constructor(parameters) {
-            
-        }
-    }]
+    icons[0].classList.add("change");
+    i = 1;
+  } else {
+    icon.nextElementSibling.classList.add("change");
   }
-  icon.nextElementSibling.classList.add("change");
 }, 4000);
