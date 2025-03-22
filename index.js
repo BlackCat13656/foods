caaa94068a3b15bb2d781a11e876737ac865a485
Ekan <ekanessien@gmail.com>
2025-03-21 21:33:25 -0700

    Advances in CSS and some JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const icons = document.querySelectorAll(".section-1-icons i");
+
+let i = 1;
+
+setInterval(() => {
+  i++;
+
+  const icon = document.querySelector(".section-1-icons .change");
+  icon.classList.remove("change");
+
+  if (i > icons.length) {
+    icons[0.classList.add("change") {
+        constructor(parameters) {
+            
+        }
+    }]
+  }
+  icon.nextElementSibling.classList.add("change");
+}, 4000);
